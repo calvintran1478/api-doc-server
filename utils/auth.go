05ad0c2666ba2fcd00109545b31cfa6d70842c96
Calvin Tran <calvintran1478@protonmail.com>
2026-03-22 20:23:02 -0400

    Redirect to login page when unauthorized

--- utils/auth.go
+++ utils/auth.go
@@ -7,11 +7,7 @@ import (
 func GetUser(w http.ResponseWriter, r *http.Request) string {
 	cookie, err := r.Cookie("access-token")
 	if err != nil {
-		if err == http.ErrNoCookie {
-			http.Error(w, "Cookie not found", http.StatusUnauthorized)
-			return ""
-		}
-		http.Error(w, "", http.StatusInternalServerError)
+		http.Redirect(w, r, "/login.html", http.StatusFound)
 		return ""
 	}
 

6ec7f16754be51738c929849fec581731ba1c3e3
Calvin Tran <calvintran1478@protonmail.com>
2026-04-20 22:37:36 -0400

    Add queries for creating endpoint related tables

--- setup.go
+++ setup.go
@@ -53,4 +53,119 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error creating project table", err)
 		os.Exit(1)
 	}
+
+	// Create endpoint table
+	endpointTableQuery := `
+		CREATE TABLE IF NOT EXISTS endpoints (
+			endpoint_id VARCHAR PRIMARY KEY,
+			method VARCHAR NOT NULL,
+			path VARCHAR NOT NULL,
+			description VARCHAR NOT NULL,
+			project_id VARCHAR,
+			CONSTRAINT endpoint_project_id_fkey FOREIGN KEY(project_id) REFERENCES projects(project_id)
+				ON DELETE CASCADE
+				ON UPDATE CASCADE
+		);
+	`
+	_, err = pool.Exec(context.Background(), endpointTableQuery)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating endpoint table", err)
+		os.Exit(1)
+	}
+
+	// Create path parameter table
+	pathParameterTableQuery := `
+		CREATE TABLE IF NOT EXISTS path_parameters (
+			name VARCHAR NOT NULL,
+			type VARCHAR NOT NULL,
+			description VARCHAR NOT NULL,
+			endpoint_id VARCHAR,
+			CONSTRAINT path_parameter_endpoint_id_fkey FOREIGN KEY(endpoint_id) REFERENCES endpoints(endpoint_id)
+				ON DELETE CASCADE
+				ON UPDATE CASCADE,
+			PRIMARY KEY(name, endpoint_id)
+		);
+	`
+	_, err = pool.Exec(context.Background(), pathParameterTableQuery)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating path parameter table", err)
+		os.Exit(1)
+	}
+
+	// Create query parameter table
+	queryParameterTableQuery := `
+		CREATE TABLE IF NOT EXISTS query_parameters (
+			name VARCHAR NOT NULL,
+			type VARCHAR NOT NULL,
+			description VARCHAR NOT NULL,
+			required BOOLEAN NOT NULL,
+			endpoint_id VARCHAR,
+			CONSTRAINT query_parameter_endpoint_id_fkey FOREIGN KEY(endpoint_id) REFERENCES endpoints(endpoint_id)
+				ON DELETE CASCADE
+				ON UPDATE CASCADE,
+			PRIMARY KEY(name, endpoint_id)
+		);
+	`
+	_, err = pool.Exec(context.Background(), queryParameterTableQuery)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating query parameter table", err)
+		os.Exit(1)
+	}
+
+	// Create request body fields table
+	requestBodyFieldsTableQuery := `
+		CREATE TABLE IF NOT EXISTS request_body_fields (
+			name VARCHAR NOT NULL,
+			type VARCHAR NOT NULL,
+			description VARCHAR NOT NULL,
+			required BOOLEAN NOT NULL,
+			endpoint_id VARCHAR,
+			CONSTRAINT request_body_field_endpoint_id_fkey FOREIGN KEY(endpoint_id) REFERENCES endpoints(endpoint_id)
+				ON DELETE CASCADE
+				ON UPDATE CASCADE,
+			PRIMARY KEY(name, endpoint_id)
+		);
+	`
+	_, err = pool.Exec(context.Background(), requestBodyFieldsTableQuery)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating request body fields table", err)
+		os.Exit(1)
+	}
+
+	// Create headers table
+	headersTableQuery := `
+		CREATE TABLE IF NOT EXISTS headers (
+			name VARCHAR NOT NULL,
+			description VARCHAR NOT NULL,
+			required BOOLEAN NOT NULL,
+			endpoint_id VARCHAR,
+			CONSTRAINT header_endpoint_id_fkey FOREIGN KEY(endpoint_id) REFERENCES endpoints(endpoint_id)
+				ON DELETE CASCADE
+				ON UPDATE CASCADE,
+			PRIMARY KEY(name, endpoint_id)
+		);
+	`
+	_, err = pool.Exec(context.Background(), headersTableQuery)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating headers table", err)
+		os.Exit(1)
+	}
+
+	// Create status code table
+	statusCodeTableQuery := `
+		CREATE TABLE IF NOT EXISTS status_codes (
+			code INTEGER NOT NULL,
+			description VARCHAR NOT NULL,
+			endpoint_id VARCHAR,
+			CONSTRAINT status_code_endpoint_id_fkey FOREIGN KEY(endpoint_id) REFERENCES endpoints(endpoint_id)
+				ON DELETE CASCADE
+				ON UPDATE CASCADE,
+			PRIMARY KEY(code, endpoint_id)
+		);
+	`
+	_, err = pool.Exec(context.Background(), statusCodeTableQuery)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating status codes table", err)
+		os.Exit(1)
+	}
 }
